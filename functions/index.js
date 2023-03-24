008dc18841186c356819bd5f3ccb314899ad8e49
isaackoz <isaackoz130@gmail.com>
2023-03-23 23:41:43 -0700

    update whitelist

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,22 @@
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const corsAnywhere = require('cors-anywhere');
-const cors = require("cors")({origin:true})
+const cors = require("cors")({ origin: true })
 const corsServer = corsAnywhere.createServer({
-    originWhitelist: [
-      'http://localhost:3000',
-    ],
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2'],
+  originWhitelist: [
+    'http://localhost:3000',
+    'https://www.traderdash.app',
+    'https://traderdash.app',
+    'https://traderdash.vercel.app/',
+    'https://www.traderdash.vercel.app/',
+    'https://qwopapps.vercel.app/',
+    'http://admin.qwopcapital.com/'
+  ],
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: ['cookie', 'cookie2'],
 });
 
 exports.proxy = onRequest((request, response) => {
-  cors(request,response,() =>{
+  cors(request, response, () => {
     corsServer.emit('request', request, response);
-  })    
+  })
 });
